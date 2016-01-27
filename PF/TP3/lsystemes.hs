7fd72d93fbc156e0c050c7b16aa26fc4e2e4d043
Nicolas Svirchevsky <n.svirchevsky@gmail.com>
2016-01-27 16:59:31 +0100

    TP3

---
+++ PF/TP3/lsystemes.hs
@@ -0,0 +1,30 @@
+type Symbole  = Char
+type Mot      = [Symbole]
+type Axiome   = Mot
+type Regles   = Symbole -> Mot
+type LSysteme = [Mot]
+
+motSuivant :: Regles -> Mot -> Mot
+
+motSuivant rules (a:as) = rules a ++ motSuivant rules as
+motSuivant _ [] = []
+
+motSuivant' rules ws = [ x | w <- ws, x <- rules w]
+
+motSuivant'' = concatMap
+
+kochRules :: Symbole -> Mot
+kochRules 'F' = ['F','-','F','+','+','F','-','F']
+kochRules '+' = ['+']
+kochRules '-' = ['-']
+
+kochSuivant = motSuivant kochRules
+kochSuivant' = motSuivant' kochRules
+kochSuivant'' = motSuivant'' kochRules
+
+lsysteme :: Axiome -> Regles -> LSysteme
+lsysteme w rules = iterate (motSuivant rules) w
+
+lsystemeKoch = revCur lsysteme kochRules
+
+revCur f a b = f b a
