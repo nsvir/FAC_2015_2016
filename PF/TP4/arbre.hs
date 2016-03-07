0a610b2b0f2d0cfb3b7e0eee1fce28c7d3e96786
Nicolas Svirchevsky <n.svirchevsky@gmail.com>
2016-03-07 17:23:23 +0100

    arbre

--- PF/TP4/arbre.hs
+++ PF/TP4/arbre.hs
@@ -1,37 +1,62 @@
-data Arbre a c  = Empty | Node a c (Arbre c a) (Arbre c a)
+import Test.QuickCheck 
+
+data Arbre coul a  = Empty | Node coul a (Arbre coul a) (Arbre coul a)
              deriving (Show)
 
-simpleTree :: Arbre a c
-simpleTree = Node ("rouge","a")
-                  (Node ("vert", "b") Empty Empty)
-                  (Node ("bleu", "c") Empty
-                                   (Node ("orange", "d") Empty Empty))
+simpleTree :: Arbre String String
+simpleTree = Node "rouge" "a"
+                  (Node "vert"  "b" Empty Empty)
+                  (Node "bleu"  "c" Empty
+                                   (Node "orange" "d" Empty Empty))
 
-mapTree :: (a -> c -> b) -> Arbre a c -> Arbre b
+mapTree :: (c -> a -> b) -> Arbre c a -> Arbre c b
 mapTree _ Empty = Empty
-mapTree f (Node content left right) = Node (f content) (mapTree f left) (mapTree f right)
+mapTree f (Node c a left right) = Node c (f c a) (mapTree f left) (mapTree f right)
 
-foldTree :: (Content -> b -> b) -> b -> Arbre a c -> b
+foldTree :: (c -> a -> b -> b -> b) -> b -> Arbre c a -> b
 foldTree _ r Empty = r
-foldTree f e (Node content left right) = f content (foldTree f (foldTree f e right) left)
+foldTree f e (Node c a left right) = f c a (foldTree f e right) (foldTree f e left)
 
 concatTree :: Arbre a String -> String
-concatTree = foldTree (\x  y) e -> y ++ e) []
+concatTree = foldTree (\x y e f -> y ++ e ++ f) []
 
-hauteur :: Arbre a c -> Integer
+hauteur :: Arbre a c -> Int
 hauteur Empty = 0
-hauteur (Node _ left right) | rl > rr = rl + 1
-                            | otherwise = rr + 1
+hauteur (Node _ _ left right) | rl > rr = rl + 1
+                              | otherwise = rr + 1
   where rl = hauteur left
         rr = hauteur right
 
-hauteur' :: Arbre a c -> Integer
-hauteur' = foldTree (\x y -> if x > y then x else y + 1) 0 . mapTree (\_ -> 1)
+hauteur' :: Arbre a c -> Int
+hauteur' = foldTree (\x y l r -> 1 + max l r) 0
 
-taille :: Arbre a c -> Integer
+taille :: Arbre a c -> Int
 taille Empty = 0
-taille (Node _ left right) = 1 + taille left + taille right
+taille (Node _ _ left right) = 1 + taille left + taille right
 
-taille' :: Arbre a c -> Integer
-taille' = foldTree (\_ e -> e + 1) 0
+taille' :: Arbre a c -> Int
+taille' = foldTree (\_ e l r -> 1 + l + r) 0
 
+peigneGauche :: [(c, a)] -> Arbre c a
+peigneGauche [] = Empty
+peigneGauche ((c,a):xs) = Node c a (peigneGauche xs) Empty
+
+-- Il vérifie que la hauteur de notre arbre est égale
+-- à la longueur de la liste envoyé à peigneGauche
+prop_hauteurPeigne xs = length xs == hauteur (peigneGauche xs)
+
+prop_taille xs = length xs == taille (peigneGauche xs)
+                 
+estComplet :: Arbre c a -> Bool
+estComplet Empty = True
+estComplet (Node _ _ Empty Empty) = True
+estComplet (Node _ _ _ Empty) = False
+estComplet (Node _ _ Empty _) = False
+estComplet (Node _ _ left right) = estComplet left && estComplet right
+
+complet :: (Int n) => n -> [(a, c)] -> Arbre a c
+complet 0 _ = error ""
+complet 1 [] = error ""
+complet 1 ((a, c):[]) = Node a c Empty Empty
+complet h ((x, y):xs) = Node x y (complet (h - 1) xs) (complet (h - 1)
+                                                       (drop ((2 ** h) - 1) xs))
